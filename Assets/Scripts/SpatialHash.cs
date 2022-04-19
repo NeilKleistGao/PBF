73a5dd80f411c22c7f03b8bb5ab581efbc849ff9
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-19 13:49:18 +0800

    Replace fluid particles with index

--- Assets/Scripts/SpatialHash.cs
+++ Assets/Scripts/SpatialHash.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpatialHash : MonoBehaviour {
-    private Dictionary<Vector3Int, List<FluidParticle>> mHash;
+    private Dictionary<Vector3Int, List<int>> mHash;
 
     private static SpatialHash gInstance = null;
 
@@ -14,25 +14,25 @@ public class SpatialHash : MonoBehaviour {
     }
 
     private void Awake() {
-        mHash = new Dictionary<Vector3Int, List<FluidParticle>>();
+        mHash = new Dictionary<Vector3Int, List<int>>();
         if (gInstance == null) {
             gInstance = this;
         }
     }
 
-    public void AddParticle(FluidParticle pParticle) {
-        Vector3Int hash = GetHasing(pParticle.transform.position);
+    public void AddParticle(int pIndex, Vector3Int pPosition) {
+        Vector3Int hash = GetHasing(pPosition);
         if (!mHash.ContainsKey(hash)) {
-            mHash.Add(hash, new List<FluidParticle>());
+            mHash.Add(hash, new List<int>());
         }
 
-        mHash[hash].Add(pParticle);
+        mHash[hash].Add(pIndex);
     }
 
-    public void RemoveParticle(FluidParticle pParticle) {
-        Vector3Int hash = GetHasing(pParticle.transform.position);
+    public void RemoveParticle(int pIndex, Vector3Int pPosition) {
+        Vector3Int hash = GetHasing(pPosition);
         if (mHash.ContainsKey(hash)) {
-            mHash[hash].Remove(pParticle);
+            mHash[hash].Remove(pIndex);
         }
     }
 
@@ -42,17 +42,19 @@ public class SpatialHash : MonoBehaviour {
             Mathf.FloorToInt(pPosition.z + 0.5f));
     }
 
-    public List<FluidParticle> GetNeighbors(FluidParticle pParticle) {
-        List<FluidParticle> neighbors = new List<FluidParticle>();
-        Vector3Int index = GetHasing(pParticle.transform.position);
-        
+    public List<int> GetNeighbors(int pIndex, Vector3Int pPosition) {
+        List<int> neighbors = new List<int>();
+        Vector3Int index = GetHasing(pPosition);
+
         for (int i = -1; i < 2; ++i) {
             for (int j = -1; j < 2; ++j) {
                 for (int k = -1; k < 2; ++k) {
                     Vector3Int next = new Vector3Int(index.x + i, index.y + j, index.z + k);
                     if (mHash.ContainsKey(next)) {
-                        foreach (FluidParticle fp in mHash[next]) {
-                            neighbors.Add(fp);
+                        foreach (int id in mHash[next]) {
+                            if (id != pIndex) {
+                                neighbors.Add(id);
+                            }
                         }
                     }
                 }
