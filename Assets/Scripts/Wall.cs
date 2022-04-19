060def897c8e67cd6c98cd3786baa502f517218a
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-19 11:15:03 +0800

    Add wall collision

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+    [SerializeField] private Vector3 mNormal;
+
+    private void Awake() {
+        mNormal.Normalize();
+    }
+
+    public void Collide(ref Vector3 pPosition, ref Vector3 pVelocity) {
+        float sdf = Vector3.Dot(mNormal, pPosition - transform.position);
+        if (sdf > 0) {
+            return;
+        }
+
+        pVelocity -= (sdf * mNormal) / Time.fixedDeltaTime;
+        pPosition -= sdf * mNormal;
+    }
+}
