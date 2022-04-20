6f40289fb241eea3fc6df744bd006d51ee83e351
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-20 09:31:15 +0800

    Rewrite collision handling

--- Assets/Scripts/Fluid.cs
+++ Assets/Scripts/Fluid.cs
@@ -15,10 +15,14 @@ public class Fluid : MonoBehaviour {
 
     [SerializeField] private Vector3 mRectMax;
 
+    [SerializeField] private Wall[] mWalls;
+
     private int mParticlesNumber;
     private Vector3[] mPosition;
     private Vector3[] mPredictionPosition;
     private Vector3[] mVelocity;
+    private Vector3[] mWallsNormal;
+    private Vector3[] mWallsPosition;
 
     private Bounds mBounds;
 
@@ -26,9 +30,12 @@ public class Fluid : MonoBehaviour {
 
     private ComputeBuffer mPredictionPositionBuffer;
     private ComputeBuffer mVelocityBuffer;
+    private ComputeBuffer mWallsNormalBuffer;
+    private ComputeBuffer mWallsPositionBuffer;
 
     private int mApplyForceKernel;
     private int mUpdatePositionAndVelocityKernel;
+    private int mHandleCollisionKernel;
 
     private void InitParticles() {
         int xWidth = Mathf.FloorToInt((mRectMax.x - mRectMin.x) * transform.localScale.x / mRadius / 2);
@@ -59,17 +66,32 @@ public class Fluid : MonoBehaviour {
         mMaterial.SetFloat("scale", mRadius * 2);
     }
 
+    void InitWalls() {
+        mWallsNormal = new Vector3[mWalls.Length];
+        mWallsPosition = new Vector3[mWalls.Length];
+
+        for (int i = 0; i < mWalls.Length; ++i) {
+            mWallsNormal[i] = mWalls[i].mNormal;
+            mWallsPosition[i] = mWalls[i].transform.position;
+        }
+    } 
+
     void InitComputeShader() {
         mApplyForceKernel = mSolver.FindKernel("ApplyForce");
         mUpdatePositionAndVelocityKernel = mSolver.FindKernel("UpdatePositionAndVelocity");
+        mHandleCollisionKernel = mSolver.FindKernel("HandleCollision");
 
         mPositionBuffer = new ComputeBuffer(mParticlesNumber, 12);
         mPredictionPositionBuffer = new ComputeBuffer(mParticlesNumber, 12);
         mVelocityBuffer = new ComputeBuffer(mParticlesNumber, 12);
+        mWallsNormalBuffer = new ComputeBuffer(mWalls.Length, 12);
+        mWallsPositionBuffer = new ComputeBuffer(mWalls.Length, 12);
 
         mPositionBuffer.SetData(mPosition);
         mPredictionPositionBuffer.SetData(mPredictionPosition);
         mVelocityBuffer.SetData(mVelocity);
+        mWallsNormalBuffer.SetData(mWallsNormal);
+        mWallsPositionBuffer.SetData(mWallsPosition);
 
         mSolver.SetBuffer(mApplyForceKernel, "gPosition", mPositionBuffer);
         mSolver.SetBuffer(mApplyForceKernel, "gPredictionPosition", mPredictionPositionBuffer);
@@ -78,15 +100,23 @@ public class Fluid : MonoBehaviour {
         mSolver.SetBuffer(mUpdatePositionAndVelocityKernel, "gPosition", mPositionBuffer);
         mSolver.SetBuffer(mUpdatePositionAndVelocityKernel, "gPredictionPosition", mPredictionPositionBuffer);
         mSolver.SetBuffer(mUpdatePositionAndVelocityKernel, "gVelocity", mVelocityBuffer);
+
+        mSolver.SetBuffer(mHandleCollisionKernel, "gPredictionPosition", mPredictionPositionBuffer);
+        mSolver.SetBuffer(mHandleCollisionKernel, "gVelocity", mVelocityBuffer);
+        mSolver.SetBuffer(mHandleCollisionKernel, "gWallsNormal", mWallsNormalBuffer);
+        mSolver.SetBuffer(mHandleCollisionKernel, "gWallsPosition", mWallsPositionBuffer);
     }
 
     private void Awake() {
         InitParticles();
+        InitWalls();
         InitComputeShader();
     }
 
     private void Update() {
         mSolver.SetInt("gParticlesNumber", mParticlesNumber);
+        mSolver.SetInt("gWallSize", mWalls.Length);
+        mSolver.SetFloat("gRadius", mRadius);
 
         // apply force
         {
@@ -97,7 +127,13 @@ public class Fluid : MonoBehaviour {
 
         // TODO: find neighbors
         // TODO: calculate lambda
-        // TODO: collision detection
+        // collision detection
+        {
+            int blockSize = Mathf.CeilToInt(mParticlesNumber / 1024.0f);
+            mSolver.SetInt("gBlockSize", blockSize);
+            mSolver.Dispatch(mHandleCollisionKernel, blockSize, 1, 1);
+        }
+
         // TODO: update position
         // TODO: apply viscocity
         // update position and velocity
